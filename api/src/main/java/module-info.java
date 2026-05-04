3e766b3c77ce51a356d46a915eadf5e147f7a322
Thorsten Heit <thorsten.heit@dialogika.de>
2026-05-04 16:33:01 +0200

    first draft

---
+++ api/src/main/java/module-info.java
@@ -0,0 +1,47 @@
+/*
+ * JBoss, Home of Professional Open Source
+ *
+ * Copyright 2025 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+module org.xnio.api {
+	exports org.xnio;
+	exports org.xnio.channels;
+	exports org.xnio.conduits;
+	exports org.xnio.http;
+	exports org.xnio.management;
+	exports org.xnio.sasl;
+	exports org.xnio.ssl;
+	exports org.xnio.streams;
+	exports org.xnio._private to org.jboss.logging;
+	opens org.xnio._private to org.jboss.logging;
+
+	requires java.logging;
+	requires java.management;
+	requires java.security.sasl;
+	requires java.xml;
+	requires org.jboss.logging;
+	requires org.jboss.logging.annotations;
+	requires org.jboss.threads;
+	requires org.wildfly.common;
+	requires osgi.core;
+	requires wildfly.client.config;
+	requires java.compiler;
+
+	uses org.xnio.XnioProvider;
+	uses org.xnio.XnioWorkerConfigurator;
+	
+	uses javax.security.sasl.SaslClientFactory;
+	uses javax.security.sasl.SaslServerFactory;
+}
